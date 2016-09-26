565ac00829d1ca1850ef8f061fb2ed45c2080722
SeungpilPark <sppark@uengine.org>
2016-09-26 11:41:34 +0900

    xml2json

--- src/main/webapp/doosan/aras.js
+++ src/main/webapp/doosan/aras.js
@@ -4,37 +4,16 @@
 function XML2jsobj(node) {
 
     var data = {};
-
-    // append a value
-    function Add(name, value) {
-        if (data[name]) {
-            if (data[name].constructor != Array) {
-                data[name] = [data[name]];
-            }
-            data[name][data[name].length] = value;
-        }
-        else {
-            data[name] = value;
-        }
-    };
-
-    // element attributes
-    var c, cn;
-
-    // child elements
-    for (c = 0; cn = node.childNodes[c]; c++) {
-        if (cn.nodeType == 1) {
-            if (cn.childNodes.length == 1 && cn.firstChild.nodeType == 3) {
-                // text value
-                Add(cn.nodeName, cn.firstChild.nodeValue);
-            }
-            else {
-                // sub-object
-                Add(cn.nodeName, XML2jsobj(cn));
+    var item;
+    if (node && node.childNodes && node.childNodes[0] &&
+        node.childNodes[0].childNodes && node.childNodes[0].childNodes[0]) {
+        item = node.childNodes[0].childNodes[0];
+        if (item) {
+            for (var i = 0; i < item.childNodes.length; i++) {
+                data[item.childNodes[0].nodeValue] = item.childNodes[0].firstChild.nodeValue;
             }
         }
     }
-
     return data;
 
 }
